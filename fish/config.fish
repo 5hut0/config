7941a99d5f209e6802548cd0f975c910307527bc
Yotaro Shuto <yotaro.shuto@gmail.com>
2019-07-30 10:36:31 +0900

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,25 @@
-# set PATH ~/.config/bin ~/.config/local/bin $HOME/.cargo/bin $PATH
+# path
 set PATH ~/.config/bin ~/.config/local/bin $PATH
+set PATH ~/.nodebrew/current/bin $PATH
+set NODEBREW_ROOT /usr/local/var/nodebrew
+
+# tig
 abbr ta 'tig --all'
 abbr ts 'tig status'
+
+# Sketch
 abbr sketchtool '/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool'
 abbr sketchmigrate '/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchmigrate'
+
+# Editor
 alias vi='vim'
 set -U EDITOR vim
 set -U VISUAL vim
 
+# fzf
 set -U FZF_DEFAULT_COMMAND 'ag --nocolor -g ""'
 set -U FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -U FZF_ALT_C_COMMAND "$FZF_DEFAULT_COMMAND"
-# set -U FZF_DEFAULT_OPTS '--color dark,hl:33,hl+:37,fg+:235,bg+:136,fg+:254 --color info:254,prompt:37,spinner:108,pointer:235,marker:235'
 set -U FZF_DEFAULT_OPTS '--color fg:-1,bg:-1,hl:33,fg+:254,bg+:235,hl+:33 --color info:136,prompt:136,pointer:230,marker:230,spinner:136'
 set -U FZF_TMUX 1
 
